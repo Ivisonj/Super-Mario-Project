268d25dc0b35516b4aa53650f634e2f45af434c4
Ivisonj <ivison_j@outlook.com>
2023-02-14 22:11:47 -0300

    Iniciando JS

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const botaoTrailer = document.querySelector(".trailer-button");
+const modal = document.querySelector(".modal");
+
+botaoTrailer.addEventListener("click", () => {
+    console.log('clicou!');
+})
+
